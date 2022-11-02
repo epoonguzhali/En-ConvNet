5514dedce98c0aba6cc563beb3df5e50dba8f0ab
Poonguzhali Elangovan <79298226+epoonguzhali@users.noreply.github.com>
2022-11-02 21:10:19 +0530

    Add files via upload

---
+++ EfficientData3.m
@@ -0,0 +1,248 @@
+clc;
+clear all;
+close all;
+
+ sigmoidLayer
+
+%%
+tic;
+
+% matlabroot = 'LAGDatabase'
+% Datasetpath = fullfile(matlabroot)
+% Data  = imageDatastore(Datasetpath,'IncludeSubfolders',true,'LabelSource','foldernames')
+% 
+% 
+% %Split the images from Data in the ratio 80:20 
+% [Data_G80, Data_G20] = splitEachLabel(Data,0.7,'Include','glaucoma')
+% [Data_N80, Data_N20] = splitEachLabel(Data,0.7,'Include','normal')
+% 
+% % Final Training set
+% FinalTrain = imageDatastore(cat(1,Data_G80.Files,Data_N80.Files))
+% FinalTrain.Labels = cat(1,Data_G80.Labels,Data_N80.Labels)
+% % 
+% % % Final Testing set
+% FinalTest = imageDatastore(cat(1,Data_G20.Files,Data_N20.Files));
+% FinalTest.Labels = cat(1,Data_G20.Labels,Data_N20.Labels)
+
+%%
+% Dataset 3
+
+matlabroot = 'Dataset_3Ori'
+Datasetpath = fullfile(matlabroot)
+Data  = imageDatastore(Datasetpath,'IncludeSubfolders',true,'LabelSource','foldernames')
+
+
+%Split the glaucoma images from Data in the ratio 70:30 and normal images also 
+[Data_GA7, Data_GA3] = splitEachLabel(Data,0.7,'Include','glaucoma_A1')
+[Data_GE7, Data_GE3] = splitEachLabel(Data,0.7,'Include','glaucoma_E1')
+[Data_N7, Data_N3] = splitEachLabel(Data,0.7,'Include','normal1')
+% 
+
+%Final Training set
+FinalTrain = imageDatastore(cat(1,Data_GA7.Files,Data_GE7.Files,Data_N7.Files))
+FinalTrain.Labels = cat(1,Data_GA7.Labels,Data_GE7.Labels,Data_N7.Labels)
+
+% Final Testing set
+FinalTest = imageDatastore(cat(1,Data_GA3.Files,Data_GE3.Files,Data_N3.Files))
+FinalTest.Labels = cat(1,Data_GA3.Labels,Data_GE3.Labels,Data_N3.Labels)
+
+
+% matlabroot  = 'DrishtiTrainingRotated'
+% Datasetpath = fullfile(matlabroot)
+% FinalTrain  = imageDatastore(Datasetpath,'IncludeSubfolders',true,'LabelSource','foldernames')
+% 
+% matlabroot  = 'DrishtiTestingRotated'
+% Datasetpath = fullfile(matlabroot)
+% FinalTest   = imageDatastore(Datasetpath,'IncludeSubfolders',true,'LabelSource','foldernames')
+% 
+% Finding number of images in each category for training
+labelCountTrain = countEachLabel(FinalTrain)
+
+% Finding number of images in each category for testing
+labelCountTest = countEachLabel(FinalTest)
+
+
+
+% Resize the images to the input size of first layer
+inputSize = [224,224,3];
+
+augimdsTest = augmentedImageDatastore(inputSize(1:2),FinalTest);
+
+augimdsTrain = augmentedImageDatastore(inputSize(1:2),FinalTrain);
+
+% Load the pre-trained network
+
+net = efficientnetb0;
+% 
+analyzeNetwork(net)
+
+%net.Layers(1)
+
+% Replace the Final Layers
+
+if isa(net,'SeriesNetwork') 
+  lgraph = layerGraph(net.Layers); 
+else
+  lgraph = layerGraph(net);
+end 
+
+[learnableLayer,classLayer] = findLayersToReplace(lgraph);
+[learnableLayer,classLayer] 
+
+numClasses = 3;
+
+if isa(learnableLayer,'nnet.cnn.layer.FullyConnectedLayer')
+    newLearnableLayer = fullyConnectedLayer(numClasses, ...
+        'Name','new_fc', ...
+        'WeightLearnRateFactor',10, ...
+        'BiasLearnRateFactor',10);
+    
+elseif isa(learnableLayer,'nnet.cnn.layer.Convolution2DLayer')
+    newLearnableLayer = convolution2dLayer(1,numClasses, ...
+        'Name','new_conv', ...
+        'WeightLearnRateFactor',10, ...
+        'BiasLearnRateFactor',10);
+end
+
+lgraph = replaceLayer(lgraph,learnableLayer.Name,newLearnableLayer);
+
+% Replace the classification Layer with new
+newClassLayer = classificationLayer('Name','new_classoutput');
+lgraph = replaceLayer(lgraph,classLayer.Name,newClassLayer);
+
+% Analyze the network for new Layers
+analyzeNetwork(lgraph)
+
+% Specify the training Options for Network
+options = trainingOptions('adam', ...
+    'MiniBatchSize',32, ...
+    'MaxEpochs',10, ...   
+    'InitialLearnRate',1e-4,'Shuffle','every-epoch');
+
+MyNet = trainNetwork(augimdsTrain,lgraph,options);
+save('EfficientNetDataset3(Ori).mat')
+ toc;
+ 
+  %%
+  tic;
+load('Resnet18Rimone3.mat')
+%MyNet = net;
+% 
+% % Classification validation
+%[YPred,scores] = classify(MyNet,augimdsTest);
+[YPred,scores] = classify(MyNet,augimdsTest);
+[YPredTrain,scoresTrain] = classify(MyNet,augimdsTrain);
+xlswrite('Resnet18Rim3TrainScores', scoresTrain,1);
+
+%Accuracy calculation
+YValidation = FinalTest.Labels;
+accuracy = mean(YPred == YValidation)
+
+
+% Plot confusion matrix
+figure, plotconfusion(YValidation,YPred)
+%%
+writematrix(YPredTrain,'EfficientDataset3RmsPRop_YPred.txt','Delimiter','tab')
+writematrix(scoresTrain,'EfficientDataset3RmsProp_scores.txt','Delimiter','tab')
+
+%%
+
+
+% Grad cam visualizati0
+Img1 = readimage(FinalTest,30);
+%Img1 = imresize(Img1,[64,64]);
+Img1 = imresize(Img1,[224,224]);
+figure(1),imshow(Img1)
+
+
+label_Img1 = classify(MyNet,Img1);
+
+% Visulaization using Grad-CAM
+scoreMap_Img1 = gradCAM(MyNet,Img1,label_Img1);
+%tic;
+
+figure(2)
+imshow(Img1)
+hold on
+imagesc(scoreMap_Img1,'AlphaData',0.5)
+colormap jet
+
+%%
+
+
+writematrix(YPred,'YPredRimoneDLEp5.txt','Delimiter','tab')
+writematrix(scores,'scoresRimoneDLEp5.txt','Delimiter','tab')
+% 
+toc;
+%  
+
+% Extract the features from fc7 layer
+layer = 'efficientnet-b0|model|head|global_average_pooling2d|GlobAvgPool'
+%layer = 'pool5-drop_7x7_s1';
+featuresTrainR = activations(MyNet,augimdsTrain,layer,'OutputAs','rows');
+featuresTestR = activations(MyNet,augimdsTest,layer,'OutputAs','rows');
+
+ YTrain = FinalTrain.Labels;
+ YTest = FinalTest.Labels;
+ 
+ % Classifiaction using SVM classifier
+%Create a template for SVM classfier and use Gaussian kernel funcion
+  %
+  tic;
+ 
+ mdl = fitcecoc(featuresTrainR,YTrain);
+ 
+ [YPred1,scores1] = predict(mdl,featuresTestR);
+ 
+ accuracy = mean(YPred1 ==YTest);
+ 
+ figure, plotconfusion(YTest,YPred1)
+ writematrix(YPred1,'YPredRimoneDLEp5SVMSVM.txt','Delimiter','tab')
+writematrix(scores1,'scoresRimoneDLEp5.txt','Delimiter','tab')
+ 
+ %t = templateSVM('KernelFunction','rbf') 
+%mdl = fitcecoc(featuresTrainR,YTrain,'Learners',t);
+ toc;
+ 
+ % KNN CLassifier
+ tic;
+ 
+ md2 =  fitcknn(featuresTrainR,YTrain);
+ 
+ [YPred2,scores2] = predict(md2,featuresTestR);
+ 
+ accuracy = mean(YPred2 ==YTest);
+ 
+ figure, plotconfusion(YTest,YPred2)
+ 
+ toc;
+ writematrix(YPred2,'YPredRimoneDLEp5KNN.txt','Delimiter','tab')
+writematrix(scores2,'scoresRImoneDLEp5KNN.txt','Delimiter','tab')
+% Naive Bayes CLassifier
+ tic;
+ 
+ md3 =  fitcnb(featuresTrainR,YTrain);
+ 
+ [YPred3,scores3] = predict(md3,featuresTestR);
+ 
+ accuracy = mean(YPred3 ==YTest);
+ 
+ figure, plotconfusion(YTest,YPred3)
+ 
+ toc;
+ writematrix(YPred3,'YPredRimoneDLEp5NB.txt','Delimiter','tab')
+writematrix(scores3,'scoresRimoneDLEp5NB.txt','Delimiter','tab')
+ 
+ % Decision tree
+ tic;
+ 
+ mdl =  fitctree(featuresTrainR,YTrain);
+ 
+ YPred = predict(mdl,featuresTestR);
+ 
+ accuracy = mean(YPred ==YTest);
+ 
+ figure(1), plotconfusion(YTest,YPred)
+ 
+ toc;
+ 
---
+++ EnsembleCode.m
@@ -0,0 +1,316 @@
+clc;
+clear all;
+
+%%
+
+% Accuracy for Rimone3 FRFCM results
+
+Traindata = xlsread('RimoneCDRTrain.xlsx');
+
+Testdata = xlsread('RimoneCDRTest.xlsx');
+
+TrainLabel = xlsread('RimoneLabelTrain.xlsx');
+
+TestLabel = xlsread('RimoneLabelTest.xlsx');
+
+
+%SVM1 classifier
+tic;
+ md1 = fitcsvm(Traindata,TrainLabel);
+ 
+ [YPred, scores]= predict( md1,Testdata);
+ 
+ accuracy = mean(YPred ==TestLabel);
+ 
+ %figure, plotconfusion(TestLabel,YPred)
+ CM = confusionmat(YPred,TestLabel);
+ AR = (trace(CM)./37)*100;
+ %%
+ 
+ % RImone Cup test
+ Traindata = xlsread('RimonecupTrain.xlsx');
+
+Testdata = xlsread('RimonecupTest.xlsx');
+
+TrainLabel = xlsread('RimoneLabelTrain.xlsx');
+
+TestLabel = xlsread('RimoneLabelTest.xlsx');
+
+
+%SVM1 classifier
+tic;
+ md1 = fitcsvm(Traindata,TrainLabel);
+ 
+ [YPred, scores]= predict( md1,Testdata);
+ 
+ accuracy = mean(YPred ==TestLabel);
+ 
+ %figure, plotconfusion(TestLabel,YPred)
+ CM = confusionmat(YPred,TestLabel);
+ AR = (trace(CM)./37)*100;
+ %%
+ 
+toc;0
+
+A = xlsread('test.xlsx');
+
+train_data = [A(1:56,1:4);A(92:end,1:4)];
+train_class = [A(1:56,5);A(92:end,5)];
+test_data = [A(57:91,1:4)];
+test_class = [A(57:91,5)];
+
+Mdl =fitcsvm(train_data, train_class);
+
+Tested = predict(Mdl, test_data); 
+
+CM = confusionmat(test_class,Tested);
+
+AR = (trace(CM)./35)*100;
+%%
+matlabroot  = 'DrishtiTraining'
+Datasetpath = fullfile(matlabroot)
+FinalTrain  = imageDatastore(Datasetpath,'IncludeSubfolders',true,'LabelSource','foldernames')
+
+matlabroot  = 'DrishtiTesting'
+Datasetpath = fullfile(matlabroot)
+FinalTest   = imageDatastore(Datasetpath,'IncludeSubfolders',true,'LabelSource','foldernames')
+
+% Finding number of images in each category for training
+labelCountTrain = countEachLabel(FinalTrain)
+
+% Finding number of images in each category for testing
+labelCountTest = countEachLabel(FinalTest)
+
+
+YTest = FinalTest.Labels;
+YTrain = FinalTrain.Labels;
+
+ScoresTrain = xlsread('Resner18LAGRotTrain.xls');
+
+ScoresEnsem = xlsread('EnsembleLAGRotTest.xlsx');
+
+%SVM1 classifier
+tic;
+ md1 = fitcecoc(ScoresTrain,YTrain);
+ 
+ [YPredEns, scoresEns]= predict( md1,ScoresEnsem);
+ 
+ accuracy = mean(YPredEns ==YTest);
+ 
+ figure, plotconfusion(YTest,YPredEns)
+toc;
+
+xlswrite('EnsembleLAgRotOutput',scoresEns,1);
+ writematrix(YPredEns,'EnsembleLAGRotOutputYPred.txt','Delimiter','tab')
+
+%SVM2 classifier
+tic;
+ %md11 = fitcsvm(ScoresTrain,YTrain);
+ md12 = fitcsvm(ScoresTrain,YTrain,'Standardize',true,'KernelFunction','RBF',...
+    'KernelScale','auto');
+ [YPredEns2, scoresEns2]= predict( md12,ScoresEnsem);
+ 
+ accuracy = mean(YPredEns2 ==YTest);
+ 
+ figure, plotconfusion(YTest,YPredEns2)
+toc;
+xlswrite('EnsembleLAgRotOutput',scoresEns2,2);
+ writematrix(YPredEns2,'EnsembleLAGRotOutputYPredSVM2.txt','Delimiter','tab')
+
+%KNN classifier                                              
+tic;
+ md3 = fitcknn(ScoresTrain,YTrain);
+ 
+ md31 = fitcknn(ScoresTrain,YTrain,'NumNeighbors',5,'Standardize',1)
+ [YPredEns3, scoresEns3]= predict( md31,ScoresEnsem);
+ 
+ accuracy = mean(YPredEns3 ==YTest);                           
+ 
+ figure, plotconfusion(YTest,YPredEns3)
+toc;
+xlswrite('EnsembleLAgRotOutput',scoresEns3,3);
+ writematrix(YPredEns3,'EnsembleLAGRotOutputOutputYPredKNN.txt','Delimiter','tab')
+
+
+ %%
+ % RIM-ONE3
+
+ matlabroot = 'RIM-ONE3Rot'
+Datasetpath = fullfile(matlabroot)
+Data  = imageDatastore(Datasetpath,'IncludeSubfolders',true,'LabelSource','foldernames')
+
+[Data_G80, Data_G20] = splitEachLabel(Data,0.7,'Include','G1')
+[Data_N80, Data_N20] = splitEachLabel(Data,0.7,'Include','N1')
+
+FinalTrain = imageDatastore(cat(1,Data_G80.Files,Data_N80.Files))
+FinalTrain.Labels = cat(1,Data_G80.Labels,Data_N80.Labels)
+
+% Final Testing set
+FinalTest = imageDatastore(cat(1,Data_G20.Files,Data_N20.Files));
+FinalTest.Labels = cat(1,Data_G20.Labels,Data_N20.Labels)
+
+ScoresTrain = xlsread('Rimone3Train.xlsx');
+
+ScoresEnsem = xlsread('Rimone3Testnew.xlsx');
+
+YTest = FinalTest.Labels;
+YTrain = FinalTrain.Labels;
+
+%SVM1 classifier
+tic;
+ md1 = fitcecoc(ScoresTrain,YTrain);
+ 
+ [YPredEns, scoresEns]= predict( md1,ScoresEnsem);
+ 
+ accuracy = mean(YPredEns ==YTest);
+ 
+ figure, plotconfusion(YTest,YPredEns)
+toc;
+%%
+xlswrite('EnsembleRIM3NewOutput',scoresEns,1);
+ writematrix(YPredEns,'EnsembleRIM3OutputNewYPred.txt','Delimiter','tab')
+
+%SVM2 classifier
+tic;
+ %md11 = fitcsvm(ScoresTrain,YTrain);
+ md12 = fitcsvm(ScoresTrain,YTrain,'Standardize',true,'KernelFunction','RBF',...
+    'KernelScale','auto');
+ [YPredEns2, scoresEns2]= predict( md12,ScoresEnsem);
+ 
+ accuracy = mean(YPredEns2 ==YTest);
+ 
+ figure, plotconfusion(YTest,YPredEns2)
+toc;
+xlswrite('EnsembleLAgRotOutput',scoresEns2,2);
+ writematrix(YPredEns2,'EnsembleLAGRotOutputYPredSVM2.txt','Delimiter','tab')
+
+%KNN classifier                                              
+tic;
+% md3 = fitcknn(ScoresTrain,YTrain);
+ 
+ md31 = fitcknn(ScoresTrain,YTrain,'NumNeighbors',5,'Standardize',1)
+ [YPredEns3, scoresEns3]= predict( md31,ScoresEnsem);
+ 
+ accuracy = mean(YPredEns3 ==YTest);                           
+ 
+ figure, plotconfusion(YTest,YPredEns3)
+toc;
+xlswrite('EnsembleLAgRotOutput',scoresEns3,3);
+ writematrix(YPredEns3,'EnsembleLAGRotOutputOutputYPredKNN.txt','Delimiter','tab')
+
+ %% Ensemble Resulsts for ACRIMA
+
+ matlabroot  = 'AcrimaTrainingRotated'
+Datasetpath = fullfile(matlabroot)
+FinalTrain  = imageDatastore(Datasetpath,'IncludeSubfolders',true,'LabelSource','foldernames')
+
+matlabroot  = 'AcrimaTestingRotated'
+Datasetpath = fullfile(matlabroot)
+FinalTest   = imageDatastore(Datasetpath,'IncludeSubfolders',true,'LabelSource','foldernames')
+
+YTest = FinalTest.Labels;
+YTrain = FinalTrain.Labels;
+
+
+ScoresTrain = xlsread('AcrimaTrain.xls');
+
+ScoresEnsem = xlsread('AcrimaTest.xlsx');
+
+
+%SVM1 classifier
+tic;
+ md1 = fitcecoc(ScoresTrain,YTrain);
+ 
+ [YPredEns, scoresEns]= predict( md1,ScoresEnsem);
+ 
+ accuracy = mean(YPredEns ==YTest);
+ 
+ figure, plotconfusion(YTest,YPredEns)
+toc;
+
+%SVM2 classifier
+tic;
+ %md11 = fitcsvm(ScoresTrain,YTrain);
+ md12 = fitcsvm(ScoresTrain,YTrain,'Standardize',true,'KernelFunction','RBF',...
+    'KernelScale','auto');
+ [YPredEns2, scoresEns2]= predict( md12,ScoresEnsem);
+ 
+ accuracy = mean(YPredEns2 ==YTest);
+ 
+ figure, plotconfusion(YTest,YPredEns2)
+toc;
+xlswrite('EnsembleLAgRotOutput',scoresEns2,2);
+ writematrix(YPredEns2,'EnsembleLAGRotOutputYPredSVM2.txt','Delimiter','tab')
+
+%KNN classifier                                              
+tic;
+% md3 = fitcknn(ScoresTrain,YTrain);
+ 
+ md31 = fitcknn(ScoresTrain,YTrain,'NumNeighbors',5,'Standardize',1)
+ [YPredEns3, scoresEns3]= predict( md31,ScoresEnsem);
+ 
+ accuracy = mean(YPredEns3 ==YTest);                           
+ 
+ figure, plotconfusion(YTest,YPredEns3)
+toc;
+xlswrite('EnsembleLAgRotOutput',scoresEns3,3);
+ writematrix(YPredEns3,'EnsembleLAGRotOutputOutputYPredKNN.txt','Delimiter','tab')
+
+ %% Ensemble results for Origa
+
+  matlabroot  = 'OrigaTrainingRotated'
+Datasetpath = fullfile(matlabroot)
+FinalTrain  = imageDatastore(Datasetpath,'IncludeSubfolders',true,'LabelSource','foldernames')
+
+matlabroot  = 'OrigaTestingRotated'
+Datasetpath = fullfile(matlabroot)
+FinalTest   = imageDatastore(Datasetpath,'IncludeSubfolders',true,'LabelSource','foldernames')
+
+YTest = FinalTest.Labels;
+YTrain = FinalTrain.Labels;
+
+
+ScoresTrain = xlsread('OrigaScoresTrain.xlsx');
+
+ScoresEnsem = xlsread('OrigaTest2.xlsx');
+
+
+%SVM1 classifier
+tic;
+ md1 = fitcecoc(ScoresTrain,YTrain);
+ 
+ [YPredEns, scoresEns]= predict( md1,ScoresEnsem);
+ 
+ accuracy = mean(YPredEns ==YTest);
+ 
+ figure, plotconfusion(YTest,YPredEns)
+toc;
+
+%SVM2 classifier
+tic;
+ %md11 = fitcsvm(ScoresTrain,YTrain);
+ md12 = fitcsvm(ScoresTrain,YTrain,'Standardize',true,'KernelFunction','RBF',...
+    'KernelScale','auto');
+ [YPredEns2, scoresEns2]= predict( md12,ScoresEnsem);
+ 
+ accuracy = mean(YPredEns2 ==YTest);
+ 
+ figure, plotconfusion(YTest,YPredEns2)
+toc;
+xlswrite('EnsembleLAgRotOutput',scoresEns2,2);
+ writematrix(YPredEns2,'EnsembleLAGRotOutputYPredSVM2.txt','Delimiter','tab')
+
+%KNN classifier                                              
+tic;
+% md3 = fitcknn(ScoresTrain,YTrain);
+ 
+ md31 = fitcknn(ScoresTrain,YTrain,'NumNeighbors',5,'Standardize',1)
+ [YPredEns3, scoresEns3]= predict( md31,ScoresEnsem);
+ 
+ accuracy = mean(YPredEns3 ==YTest);                           
+ 
+ figure, plotconfusion(YTest,YPredEns3)
+toc;
+xlswrite('EnsembleLAgRotOutput',scoresEns3,3);
+ writematrix(YPredEns3,'EnsembleLAGRotOutputOutputYPredKNN.txt','Delimiter','tab')
+
